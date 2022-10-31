b6348ceb040142049ecd73dea00d2740d52c2ccd
Guang Wang <xianggua@ualberta.ca>
2022-10-31 14:40:11 -0600

    deletecity fail

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -77,18 +77,19 @@ public class CustomList extends ArrayAdapter<City> {
         }
 
         return false;
-
-
     }
-/*
+
     public void delete(City city){
+        /*
         if (cities.contains(city)) {
             cities.remove(city);
         }
         else{
             throw new IllegalArgumentException();
+        }
+         */
     }
-
+/*
     public int countCities(){
         return cities.size();
 
